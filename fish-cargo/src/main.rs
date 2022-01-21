b751088636304260e42a11f50f221959e8ffe630
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 21:14:39 +0100

    tuple structs!

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,18 +1,7 @@
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
+struct Color(u8, u8, u8);
 
 fn main() {
-    // color: red, green, blue
-    let mut color = Color {
-        red: 255,
-        green: 0,
-        blue: 0,
-    };
-
-    color.blue = 45;
-
-    println!("{}, {}, {}", color.red, color.green, color.blue)
+    let mut red = Color(255, 0, 0);
+    red.2 = 60;
+    println!("{}, {}, {}", red.0, red.1, red.2)
 }

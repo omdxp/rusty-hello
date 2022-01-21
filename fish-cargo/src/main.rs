8b23aaba09933032030bf23a31706e8895a9fb64
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 22:18:36 +0100

    fish with vectors!

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,18 +1,16 @@
-struct Person {
-    name: String,
-    age: u8,
-}
-
-impl ToString for Person {
-    fn to_string(&self) -> String {
-        format!("{} is {} years old", self.name, self.age)
-    }
-}
-
 fn main() {
-    let omar = Person {
-        name: String::from("Omar"),
-        age: 23,
-    };
-    println!("{}", omar.to_string());
+    // let my_vector: Vec<i32> = Vec::new();
+    let mut my_vector = vec![1, 2, 3, 4];
+
+    println!("{}", my_vector[2]);
+
+    my_vector.push(5);
+
+    println!("{}", my_vector[4]);
+
+    my_vector.remove(1); // remove the element at index 1 (2)
+
+    for numver in my_vector.iter() {
+        println!("{}", numver);
+    }
 }

e809533536e2143043803076dc445e30bfd1aa39
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 18:09:43 +0100

    making comments on fish

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    // The macro bellow prints "Hello, fish!" in the console
+    /*
+        Here is the fishes in
+        the basket:
+        - salmon
+        - tuna
+    */
     println!("Hello, fish!");
 }

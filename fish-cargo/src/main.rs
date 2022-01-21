d79057b7713e56440de3d1a97875e1923c985e77
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 21:19:04 +0100

    pass fish by reference..

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,7 +1,19 @@
-struct Color(u8, u8, u8);
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
 fn main() {
-    let mut red = Color(255, 0, 0);
-    red.2 = 60;
-    println!("{}, {}, {}", red.0, red.1, red.2)
+    let blue = Color {
+        red: 0,
+        green: 0,
+        blue: 255,
+    };
+    print_color(&blue);
+    print_color(&blue)
+}
+
+fn print_color(c: &Color) {
+    println!("Color - R:{} G:{} B:{}", c.red, c.green, c.blue);
 }

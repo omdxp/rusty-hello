5d8c41ef848ccd8e03bc03be9643cbd3dd679e1f
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 18:36:56 +0100

    a lot fish in the sea...

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,17 +1,13 @@
 fn main() {
-    let n = 20;
-
-    if n < 30 {
-        println!("n is less than 30");
-    } else {
-        println!("n is greater than 30");
-    }
-
-    if n == 20 {
-        println!("n is equal to 20");
-    } else if n == 30 {
-        println!("n is equal to 30");
-    } else {
-        println!("n is not equal to 20 or 30");
+    let mut n = 0;
+    loop {
+        n += 1;
+        if n == 7 {
+            continue;
+        }
+        println!("{}", n);
+        if n == 10 {
+            break;
+        }
     }
 }

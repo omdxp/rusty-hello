c56f40ffbe6872cc30d8b39d8c3573b7a845649d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 18:17:55 +0100

    data types, fishES!!

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let mut x = 23;
-    
-    println!("x is {}", x);
-
-    x = 42;
-
-    println!("x is {}", x);
+    let x = 5; // x: i32
+    let y: i64 = 5; // y: i64
+    let f: u64 = 5; // f: u64
+    let z: f32 = 6.7; // z: f32
+    let c: char = 'c'; // c: char
+    let b: bool = true; // b: bool
 }

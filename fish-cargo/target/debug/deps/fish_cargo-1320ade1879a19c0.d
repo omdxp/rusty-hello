85c879167d7973092c7cb37816f7127529df6c40
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 18:07:09 +0100

    creating a new cargo

---
+++ fish-cargo/target/debug/deps/fish_cargo-1320ade1879a19c0.d
@@ -0,0 +1,5 @@
+/Users/Omar/Projects/Rust/rusty-hello/fish-cargo/target/debug/deps/fish_cargo-1320ade1879a19c0: src/main.rs
+
+/Users/Omar/Projects/Rust/rusty-hello/fish-cargo/target/debug/deps/fish_cargo-1320ade1879a19c0.d: src/main.rs
+
+src/main.rs:

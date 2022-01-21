f80b21c6d0018534a2a93a282d24937e2f8a840c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-22 00:45:56 +0100

    rust tests

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,10 +1,43 @@
-use std::process::Command;
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-fn main() {
-    let output = Command::new("python3")
-        .arg("fish.py")
-        .output()
-        .expect("failed to execute process");
+impl Rectangle {
+    fn is_square(&self) -> bool {
+        self.width == self.height
+    }
+}
 
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+fn main() {}
+
+fn give_two() -> i32 {
+    2
+}
+
+#[cfg(test)]
+mod fish_tests {
+    #[test]
+    #[should_panic]
+    fn it_works() {
+        assert_eq!(2 + super::give_two(), 4);
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    #[ignore]
+    fn it_works_2() {
+        assert_eq!(super::give_two() + 2, 5);
+        assert_ne!(2 + super::give_two(), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_structs() {
+        let r = super::Rectangle {
+            width: 20,
+            height: 10,
+        };
+        assert!(r.is_square());
+    }
 }
---
+++ fish-cargo/target/debug/deps/fish_cargo-cb56a29aa7ab2a3b.d
@@ -0,0 +1,5 @@
+/Users/Omar/Projects/Rust/rusty-hello/fish-cargo/target/debug/deps/fish_cargo-cb56a29aa7ab2a3b: src/main.rs
+
+/Users/Omar/Projects/Rust/rusty-hello/fish-cargo/target/debug/deps/fish_cargo-cb56a29aa7ab2a3b.d: src/main.rs
+
+src/main.rs:

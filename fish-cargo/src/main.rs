fb9895aa66b85549c29dfdb5f5c46ef0af9606f9
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 22:58:11 +0100

    hash map through fish!

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,12 +1,36 @@
-use std::io;
+use std::collections::HashMap;
 
 fn main() {
-    let mut input = String::new();
+    let mut fish = HashMap::new();
+    fish.insert(String::from("Blue"), 10);
+    fish.insert(String::from("Yellow"), 50);
 
-    println!("Enter your name:");
+    println!("{:?}", fish);
 
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => println!("Hello, {}", input.trim()),
-        Err(error) => println!("Error: {}", error),
+    // find length of HashMap
+    println!("{}", fish.len());
+
+    // get value from HashMap
+    match fish.get("Blue") {
+        Some(v) => println!("{}", v),
+        None => println!("Not found"),
+    }
+
+    // update value in HashMap
+    fish.insert(String::from("Blue"), 25);
+    println!("{:?}", fish);
+
+    // remove value from HashMap
+    fish.remove("Blue");
+    println!("{:?}", fish);
+
+    // iterate over HashMap
+    for (key, value) in &fish {
+        println!("{}: {}", key, value);
+    }
+
+    // check if key exists in HashMap
+    if fish.contains_key("Yellow") {
+        println!("Yellow exists");
     }
 }

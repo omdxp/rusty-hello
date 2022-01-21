9f2bde7cb0d7279b0e89d3658d0ba12fb7285566
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 23:25:58 +0100

    some other string methods!

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,11 +1,48 @@
-extern crate rand;
-use rand::Rng;
-
 fn main() {
-    let random_number = rand::thread_rng().gen_range(1, 11); // 1 - 10
-    println!("Random Number: {}", random_number);
+    // Replace
+    {
+        let my_string = String::from("Rust is fantastic!");
+        println!(
+            "After replace: {}",
+            my_string.replace("fantastic", "awesome")
+        );
+    }
 
-    // Flip a coin
-    // let coin = rand::thread_rng().gen_weighted_bool(2); // 50% chance of true
-    // println!("Coin: {}", coin);
+    // Lines
+    {
+        let my_string = String::from("The weather is\nnice\noutside mate!");
+
+        for line in my_string.lines() {
+            println!("[ {} ]", line)
+        }
+    }
+
+    // Split
+    {
+        let my_string = String::from("Leave+a+fish+in+the+water");
+
+        let tokens: Vec<&str> = my_string.split('+').collect();
+
+        for token in tokens {
+            println!("[ {} ]", token)
+        }
+    }
+
+    // Trim
+    {
+        let my_string = String::from("  Remove whitespace  ");
+
+        println!("[ {} ]", my_string.trim());
+    }
+
+    // Chars
+    {
+        let my_string = String::from("Fish are awesome!");
+
+        // Get the character at index
+        match my_string.chars().nth(2) {
+            Some(c) => println!("[ {} ]", c),
+            None => println!("[ None ]"),
+        }
+    }
 }

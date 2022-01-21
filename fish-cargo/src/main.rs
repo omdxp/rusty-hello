9b0e9e06afc8e2ac0410d9c547ac3df57bb3e78e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-22 00:25:20 +0100

    enum methods, can you fish on weekends?

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,25 +1,25 @@
-extern crate reqwest;
+#![allow(dead_code)]
+
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+
+impl Day {
+    fn is_weekend(&self) -> bool {
+        match self {
+            &Day::Saturday | &Day::Sunday => true,
+            _ => false,
+        }
+    }
+}
 
 fn main() {
-    let response_text = reqwest::get("http://localhost:8000/")
-        .expect("Failed to send request")
-        .text()
-        .expect("Failed to read response");
-
-    println!("{}", response_text);
-
-    match reqwest::get("http://localhost:8000/") {
-        Ok(mut response) => {
-            // check if 200 Ok
-            if response.status() == reqwest::StatusCode::Ok {
-                match response.text() {
-                    Ok(text) => println!("{}", text),
-                    Err(e) => println!("Error: {}", e),
-                }
-            } else {
-                println!("Error: {}", response.status());
-            }
-        }
-        Err(e) => println!("Error: {}", e),
-    }
+    let day = Day::Sunday;
+    println!("{}", day.is_weekend());
 }

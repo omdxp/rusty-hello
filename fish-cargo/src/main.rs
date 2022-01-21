52a9fe06ecaf5823e4cca13e70e1667746650244
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-21 18:26:48 +0100

    check the number of fish

--- fish-cargo/src/main.rs
+++ fish-cargo/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
-    let x = 5; // x: i32
-    let y: i64 = 5; // y: i64
-    let f: u64 = 5; // f: u64
-    let z: f32 = 6.7; // z: f32
-    let c: char = 'c'; // c: char
-    let b: bool = true; // b: bool
+    let n = 20;
+
+    if n < 30 {
+        println!("n is less than 30");
+    } else {
+        println!("n is greater than 30");
+    }
+
+    if n == 20 {
+        println!("n is equal to 20");
+    } else if n == 30 {
+        println!("n is equal to 30");
+    } else {
+        println!("n is not equal to 20 or 30");
+    }
 }
